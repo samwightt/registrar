bcfc261b31c8ea4f7804f1ab60bdc60a2c0fc53a
Sam Wight <samuelwight@gmail.com>
2019-12-28 17:32:27 -0600

    Add hackathon creation

---
+++ app/controllers/admin/hackathons_controller.rb
@@ -0,0 +1,37 @@
+class Admin::HackathonsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def new
+    @hackathon = Hackathon.new
+  end
+
+  def create
+    @hackathon = Hackathon.new(new_params)
+
+    respond_to do |format|
+      if @hackathon.save
+        format.html { redirect_to [:admin, @hackathon] }
+      else
+        format.js
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def new_params
+    params.require(:hackathon).permit(:name, :description, :start_date, :end_date, :status)
+  end
+end
--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -2,6 +2,8 @@ class Hackathon < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :status, presence: true
+  validates :start_date, presence: true, if: Proc.new { |a| a.end_date? }
+  validates :end_date, presence: true, if: Proc.new { |a| a.start_date? }
   
   enum status: [:draft, :published, :open, :closed, :archived]
 end
